60674ae379c0e03a7c9682ba98616d2d779f2451
Nkunim Asaah Osei <oseinkunim@gmail.com>
2024-09-16 11:44:19 +0000

    Registration Login

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// Select form ny id
+const restigerForm = document.getElementById("register-form");
+// Handle form submit event 
+restigerForm.onsubmit = function() {
+    // Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send a confirmation email
+}
